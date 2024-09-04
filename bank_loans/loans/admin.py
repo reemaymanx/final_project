8a48ac2cdd5ffe83921839aefacfb616fc15736c
reemaymanx <91758861+reemaymanx@users.noreply.github.com>
2024-09-04 17:57:36 +0300

    Delete bank_loans/loans directory

--- bank_loans/loans/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import LoanProvider, LoanCustomer, BankPersonnel, LoanApplication
-
-admin.site.register(LoanProvider)
-admin.site.register(LoanCustomer)
-admin.site.register(BankPersonnel)
-admin.site.register(LoanApplication)
